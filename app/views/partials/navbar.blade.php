86d3e1750e7f848253d6924c444053a4bf96f917
James Garcia <littler3dhouses@gmail.com>
2014-12-09 11:08:08 -0600

    Fixed links for Portfolio and Resume in Navbar.

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -14,8 +14,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="resume"><a href="http://blog.dev/resume">Resumé</a></li>
-        <li><a href="http://blog.dev/portfolio">Portfolio</a></li>
+        <li class="resume"><a href="{{ action('HomeController@showResume') }}">Resumé</a></li>
+        <li><a href="{{ action('HomeController@showPortfolio') }}">Portfolio</a></li>
         <li><a href="{{ action('PostsController@index') }}">Blog</a></li>
         @if (Auth::check())
           <li><a href="{{ action('PostsController@create') }}">New Blog Post</a></li>
