9ff66c8e18566780b63b3a1b9e17efb226c0c637
James Garcia <littler3dhouses@gmail.com>
2014-12-11 09:05:10 -0600

    The edit fell apart, so this includes some edited files to ensure it works correctly.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Laravel Blog</title>
+    <title>James' Site</title>
 	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
--- app/views/posts/edit.blade.php
+++ app/views/posts/edit.blade.php
@@ -3,26 +3,28 @@
 @section ('content')
 
 <div id="background" class="container">
-	<h2 align="center">Edit blog Posts</h2>
+	<h2 align="center">Edit Post</h2>
 		<div class="col-md-6">
-       	{{ Form::model($post, (array('action' => ['PostsController@update', $post->id], 'method' => 'PUT'))) }}
-			<div class="form-group">
-				<label for="title" class="edit-title">Title</label>
-			    <input type="text" class="form-control" name="title" value="{{{ $post->title }}}" id="blogTitle" placeholder="Title">
+        	<div class="row">
+		       	{{ Form::model($post, (array('action' => ['PostsController@update', $post->id], 'method' => 'PUT'))) }}
+					<div class="form-group">
+						<label for="title" class="edit-title">Title</label>
+					    <input type="text" class="form-control" name="title" value="{{{ $post->title }}}" id="blogTitle" placeholder="Title">
+					</div>
+					<div class="form-group">
+					    <label for="body" class="edit-body">Body</label>
+					    <textarea name="body" rows="4" class="form-control">{{{ $post->body }}}</textarea>
+					</div>
+					<button type="submit" class="btn btn-success btn-sm">Save</button>
+				{{ Form::close() }}	
 			</div>
-			<div class="form-group">
-			    <label for="body" class="edit-body">Body</label>
-			    <textarea name="body" rows="4" class="form-control">{{{ $post->body }}}</textarea>
-			</div>
-			<button type="submit" class="btn btn-info btn-sm">Edit</button>
-		{{ Form::close() }}	
-        
-        {{ Form::open(array('method' => 'delete', 'action' => ['PostsController@destroy', $post->id], 'role' => 'form', 'class' => 'form-horizontal', 'id' => 'delete-form')) }}
-                
-            {{ Form::submit('Delete!', (array('class' => 'btn btn-danger btn-sm')))}}
-        
-        {{ Form::close() }}
-
+			<div class="row"> 
+		        {{ Form::open(array('method' => 'delete', 'action' => ['PostsController@destroy', $post->id], 'role' => 'form', 'class' => 'form-horizontal', 'id' => 'delete-form')) }}
+		           
+		            {{ Form::submit('Delete!', (array('class' => 'btn btn-danger btn-sm')))}}
+		        
+		        {{ Form::close() }}
+		    </div>
 		</div>
 </div>
 
