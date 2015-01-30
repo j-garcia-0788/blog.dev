8a9dfb2c16ab9c8c7aa0e2f21fcbfd5cd911e853
James Garcia <littler3dhouses@gmail.com>
2015-01-29 18:02:02 -0600

    User authentication for the edit blog post, we don't need strangers editing that.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,19 +2,21 @@
 
 @section ('content')
 
-	<h2 class="text-center">{{ $post->title }}</h2>
-		<div class="col-md-12 text-center">	
-			<div class="row"> 
-				<div class="col-lg-12 text center">
-					<p>{{ $post->body }}</p>
-				</div>
-			</div>
-			<br>
-			<div class="col-lg-12 text-center">
-				<a class='btn btn-success btn-sm' href="{{ action('PostsController@edit', $post->id) }}">Edit Post</a>
-				<a class='btn btn-info btn-sm' href="{{ action('PostsController@index') }}">Show Blog</a>
+<h2 class="text-center">{{ $post->title }}</h2>
+	<div class="col-md-12 text-center">	
+		<div class="row"> 
+			<div class="col-lg-12 text center">
+				<p>{{ $post->body }}</p>
 			</div>
 		</div>
+		<br>
+		<div class="col-lg-12 text-center">
+			@if (Auth::check())
+				<a class='btn btn-success btn-sm' href="{{ action('PostsController@edit', $post->id) }}">Edit Post</a>
+			@endif
+			<a class='btn btn-info btn-sm' href="{{ action('PostsController@index') }}">Show Blog</a>
+		</div>
+	</div>
 </div>
 
 @stop
