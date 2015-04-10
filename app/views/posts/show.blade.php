eef9618d618b823873089320c5a01946f92c99a7
James Garcia <littler3dhouses@gmail.com>
2015-04-10 17:35:23 -0500

    Refactored the Authcheck as it was in there twice, unnecessarily.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -12,9 +12,7 @@
   		<div class="col-lg-12 text-center">
 			@if (Auth::check())
 				<a class='btn btn-success btn-sm' href="{{ action('PostsController@edit', $post->id) }}">Edit Post</a>
-			@endif
 			<a class='btn btn-info btn-sm' href="{{ action('PostsController@index') }}">Show Blog</a>
-			@if (Auth::check())
 				<a class='btn btn-danger btn-sm' href="{{ action('PostsController@destroy', $post->id) }}">Delete Post</a>
 			@endif
 		</div>
