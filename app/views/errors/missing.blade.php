57e138ddef1fcf7a829c8c9c9d8385f549d0c324
James Garcia <littler3dhouses@gmail.com>
2015-08-24 14:10:19 -0500

    Edited the 404 page to be centered and nicer looking.

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -4,8 +4,9 @@
 
 <link rel="stylesheet" href="css/style.css">
 	
-	<div class="parallax parallax-window" id="missing">
-		<h2>This page...?</h2>
-		<img src="../img/gohere.gif">
+	<div class="header" id="missing">
+		<h2 class="missingText">Oh this page...?<br>
+		  <img class="missingImage" src="../img/gohere.gif">
+        </h2>
 	</div>
 @stop
